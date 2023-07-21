2c63d0ab81fc601ef810cd12acb56ccc87f1f377
Simon Pfeiffer <simonp@codesphere.com>
2023-07-21 16:38:21 +0200

    Update hello world route

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
-    }
-    @GetMapping("/hello")
+	}
+	@GetMapping("/")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
     }
